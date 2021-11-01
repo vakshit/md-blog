806748e629db497654f3ad8a4b8eed0c345e8884
vakshit <akshitv18@gmail.com>
2021-11-02 02:53:44 +0530

    v1.0

--- server.js
+++ server.js
@@ -157,6 +157,6 @@ app.get("/loading", (req, res) => {
   res.writeProcessing("Loading");
 });
 
-app.listen("3000", () => {
+app.listen(process.env.PORT || "3000", () => {
   console.log("server is running on port 3000");
 });
